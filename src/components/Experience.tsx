1bcc389b3c5246cc3caef1beb8aeec4a52899c85
Goksyfresh <opajobioyegokeoyebola@gmail.com>
2026-01-08 12:00:01 +0100

    v1.12

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,16 +3,16 @@ import Stack from './Stack'
 
 const Experience = () => {
   return (
-    <section className='flex flex-col items-center justify-center gap-4 mt-35 px-6'>
+    <section className='flex flex-col items-center justify-center gap-4 mt-35 px-4'>
         <h2 className='[color:var(--heading_1_color)] lg:text-3xl text-xl uppercase'>Experience</h2>
         <div className='flex justify-between items-center w-[60%]'>
         <h3 className='[color:var(--heading_1_color)] lg:text-xl text-lg'>Frontend Engineer at Qorporate</h3>
         <p className='[color:var(--text-color)] uppercase text-sm '>August 2025 - present</p>
         </div>
-        <p className='[color:var(--text-color)] lg:text-lg text-sm w-[60%] text-center'>As a passionate software engineer, I thrive on the intricate dance between logic and creativity. Currently immersed in the dynamic world of WebHR, my expertise centers around React Native, where I seamlessly blend technology with innovation.
+        <p className='[color:var(--text-color)] lg:text-xl text-lg w-[80%] text-center'>As a passionate software engineer, I thrive on the intricate dance between logic and creativity. Currently immersed in the dynamic world of WebHR, my expertise centers around React Native, where I seamlessly blend technology with innovation.
 </p>
-<p className='[color:var(--text-color)] lg:text-lg text-sm w-[60%] text-center'>With a fervor for crafting elegant solutions, I navigate the ever-evolving landscape of software development. My journey involves translating concepts into code, creating seamless user experiences, and constantly pushing the boundaries of what's possible</p>
-   <div className='flex gap-4 items-center justify-center mt-4 w-[60%] flex-wrap'>
+<p className='[color:var(--text-color)] lg:text-xl text-lg w-[80%] text-center'>With a fervor for crafting elegant solutions, I navigate the ever-evolving landscape of software development. My journey involves translating concepts into code, creating seamless user experiences, and constantly pushing the boundaries of what's possible</p>
+   <div className='flex gap-4 items-center justify-center mt-4 w-[80%] flex-wrap'>
     <Stack stack="React" />
      <Stack stack="Svelte" />
         <Stack stack="TypeScript" />
--- src/components/projectComponent.tsx
+++ src/components/projectComponent.tsx
@@ -7,7 +7,7 @@ const ProjectComponent = ({img, title, description}: {img: string, title: string
     }}>
       <img src={img} className='w-[100%] h-[60%] lg:h-[80%] object-cover rounded-t-xl' alt={title} />
       <div className='flex px-3 py-4 flex-col gap-2 items-start'>
-        <h3 className='[color:var(--text-color)] text-lg lg:text-xl'>{title}</h3>
+        <h3 className='[color:var(--text-color)] text-lg lg:text-xl font-bold'>{title}</h3>
         <p className='[color:var(--text-color)] text-sm lg:text-lg'>{description}</p>
       </div>
     </div>
